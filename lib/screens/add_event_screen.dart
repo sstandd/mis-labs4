5936fc0a634e95021a1659e75f612ea41e28eea5
stanchevskid <101475851+stanchevskid@users.noreply.github.com>
2025-01-19 23:54:38 +0100

    Added time field in event creation

--- lib/screens/add_event_screen.dart
+++ lib/screens/add_event_screen.dart
@@ -19,15 +19,19 @@ class AddEventScreen extends StatefulWidget {
 
 class _AddEventScreenState extends State<AddEventScreen> {
   final _titleController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
   double _latitude = 0.0;
   double _longitude = 0.0;
   late CameraPosition _initialCameraPosition;
   bool isMapInitialized = false;
+  late DateTime _eventDateTime;
 
   @override
   void initState() {
     super.initState();
+    _eventDateTime = widget.selectedDate;
     _getCurrentLocation();
+    _timeController.text = _formatTime(_eventDateTime);
   }
 
   Future<void> _getCurrentLocation() async {
@@ -62,6 +66,32 @@ class _AddEventScreenState extends State<AddEventScreen> {
     });
   }
 
+  Future<void> _selectTime() async {
+    final TimeOfDay? selectedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(_eventDateTime),
+    );
+
+    if (selectedTime != null) {
+      setState(() {
+        _eventDateTime = DateTime(
+          _eventDateTime.year,
+          _eventDateTime.month,
+          _eventDateTime.day,
+          selectedTime.hour,
+          selectedTime.minute,
+        );
+        _timeController.text = _formatTime(_eventDateTime);
+      });
+    }
+  }
+
+  String _formatTime(DateTime dateTime) {
+    final String hours = dateTime.hour.toString().padLeft(2, '0');
+    final String minutes = dateTime.minute.toString().padLeft(2, '0');
+    return '$hours:$minutes';
+  }
+
   void _saveEvent() {
     if (_titleController.text.isEmpty || _latitude == 0.0 || _longitude == 0.0) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -72,7 +102,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
 
     final event = Event(
       title: _titleController.text,
-      dateTime: widget.selectedDate,
+      dateTime: _eventDateTime,
       latitude: _latitude,
       longitude: _longitude,
     );
@@ -94,7 +124,29 @@ class _AddEventScreenState extends State<AddEventScreen> {
           children: [
             EventTitleInput(controller: _titleController),
             const SizedBox(height: 20),
-            SelectedDateDisplay(selectedDate: widget.selectedDate),
+            SelectedDateDisplay(selectedDate: _eventDateTime),
+            const SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(
+                  color: Colors.grey.shade400,
+                  width: 1.0,
+                ),
+              ),
+              child: TextFormField(
+                controller: _timeController,
+                readOnly: true,
+                onTap: _selectTime,
+                decoration: InputDecoration(
+                  labelText: 'Event Time',
+                  prefixIcon: const Icon(Icons.access_time),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                  border: InputBorder.none,
+                  fillColor: Colors.grey[200],
+                )
+              ),
+            ),
             const SizedBox(height: 20),
             MapPicker(
               isMapInitialized: isMapInitialized,
